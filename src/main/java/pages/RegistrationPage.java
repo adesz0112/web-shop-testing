048569000d8b6eaa0bfab8b90686ca6392c23562
adesz0112 <meszaros.adam94@gmail.com>
2024-11-14 13:57:32 +0100

    Base methods created for registration

--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 
 public class RegistrationPage extends BasePage {
  @FindBy (css = "input[data-qa='signup-name']")
@@ -18,11 +19,11 @@ public class RegistrationPage extends BasePage {
  @FindBy (id = "password")
  private WebElement passwordInput;
  @FindBy (id = "days")
- private WebElement daysInput;
+ private WebElement dayDropdown;
  @FindBy (id = "months")
- private WebElement monthInput;
+ private WebElement monthDropDown;
  @FindBy (id = "years")
- private WebElement yearInput;
+ private WebElement yearDropDown;
  @FindBy (id = "first_name")
  private WebElement firstNameInput;
  @FindBy (id = "last_name")
@@ -34,7 +35,7 @@ public class RegistrationPage extends BasePage {
  @FindBy (id = "address2")
  private WebElement address2Input;
  @FindBy (id = "country")
- private WebElement countrySelector;
+ private WebElement countryDropDown;
  @FindBy (id = "state")
  private WebElement stateInput;
  @FindBy (id = "city")
@@ -61,5 +62,83 @@ public class RegistrationPage extends BasePage {
  }
  public void enterEmail (String email) {
   wait(emailInput).sendKeys(email);
+ }
+
+ public void chooseGender1 () {
+  genderButton1.click();
+ }
+ public void chooseGender2 () {
+  genderButton2.click();
+ }
+ public void enterPassword (String password) {
+  wait(passwordInput).sendKeys(password);
+ }
+ public void selectDayByValue(String value) {
+  Select select = new Select(dayDropdown);
+  select.selectByValue(value);
+ }
+
+ public void selectMonthByValue(String value) {
+  Select select = new Select(monthDropDown);
+  select.selectByValue(value);
+ }
+
+ public void selectYearByValue(String value) {
+  Select select = new Select(yearDropDown);
+  select.selectByValue(value);
+ }
+
+
+ public void enterFirstName (String firstName) {
+  wait(firstNameInput).sendKeys(firstName);
+ }
+
+ public void enterLastName (String lastName) {
+  wait(lastNameInput).sendKeys(lastName);
+ }
+
+ public void enterCompany (String company) {
+  wait(companyInput).sendKeys(company);
+ }
+
+ public void enterAddress1 (String address1) {
+  wait(address1Input).sendKeys(address1);
+ }
+
+ public void enterAddress2 (String address2) {
+  wait(address2Input).sendKeys(address2);
+ }
+
+ public void selectCountryByValue(String value) {
+  Select select = new Select(countryDropDown);
+  select.selectByValue(value);
+ }
+
+ public void enterState (String state) {
+  wait(stateInput).sendKeys(state);
+ }
+
+ public void enterCity (String city) {
+  wait(cityInput).sendKeys(city);
+ }
+
+ public void enterZipcode (String zipcode) {
+  wait(zipcodeInput).sendKeys(zipcode);
+ }
+
+ public void enterMobileNumber (String mobileNumber) {
+  wait(mobileNumberInput).sendKeys(mobileNumber);
+ }
+
+ public void clickCreateAccountButton () {
+  createAccountButton.click();
+ }
+
+ public void clickContinueButton () {
+  continueButton.click();
+ }
+
+ public void clickSignUpButton () {
+  signUpButton.click();
  }
 }
